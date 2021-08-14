f17d1eaaf517d19cfa5ba55451be05ca5e74e7ab
vinz-1203 <62464810+vinz-1203@users.noreply.github.com>
2021-08-14 16:45:46 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/*!
+* Start Bootstrap - Resume v7.0.3 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+   
+
+});
